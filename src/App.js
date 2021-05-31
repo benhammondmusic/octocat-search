b5f9a76749ff34ee218d27d0fbb6658718e47185
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-05-31 09:08:00 -0600

    adds basic routes, / displays query controls and results, /detail will show a detail view

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import QueryBox from './components/QueryBox/QueryBox';
 import RepoDetail from './components/RepoDetail/RepoDetail';
 import RepoResults from './components/RepoResults/RepoResults';
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      <QueryBox />
-      <RepoResults />
-      <RepoDetail />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <QueryBox />
+            <RepoResults />
+          </Route>
+
+          <Route path="/detail">
+            <RepoDetail />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
